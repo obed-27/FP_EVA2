55ee8448b9abb3c996a2feaba29a531bba5cc565
obed-27 <l24550299@chihuahua2.tecnm.mx>
2024-11-07 13:57:20 -0700

    Add files via upload

---
+++ EVA2_15_ARREGLOS/src/main/java/com/mycompany/eva2_15_arreglos/EVA2_15_ARREGLOS.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_15_arreglos;
+
+/**
+ *
+ * @author obeds
+ */
+public class EVA2_15_ARREGLOS {
+
+    public static void main(String[] args) {
+        String[] ds=new String[7];
+        ds[0]="DOMINGO";
+        ds[1]="LUNES";
+        ds[2]="MARTES";
+        ds[3]="MIERCOLES";
+        ds[4]="JUEVES";
+        ds[5]="VIERNES";
+        ds[6]="SABADO";
+        for (int i = 0; i < ds.length; i++){
+            System.out.println(ds[i]);           
+    }
+    }
+}
