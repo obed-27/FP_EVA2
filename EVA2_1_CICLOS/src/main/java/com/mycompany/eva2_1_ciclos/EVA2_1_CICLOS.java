afdbbdb138580d770cb85f576992f0cc5e79b3ff
obed-27 <l24550299@chihuahua2.tecnm.mx>
2024-11-07 13:48:45 -0700

    Add files via upload

---
+++ EVA2_1_CICLOS/src/main/java/com/mycompany/eva2_1_ciclos/EVA2_1_CICLOS.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_1_ciclos;
+
+/**
+ *
+ * @author obeds
+ */
+public class EVA2_1_CICLOS {
+
+    public static void main(String[] args) {
+       for (int i = 1; i <= 10; i++) {
+            System.out.println ( i);
+        }
+       System.out.println("");
+       for (int i = 10; i >= 1; i--) {
+            System.out.println ( i);
+        }
+    }
+}
