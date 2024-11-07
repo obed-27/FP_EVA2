c88522393ed418fa7dfa5d565da1f87bafc46d3e
obed-27 <l24550299@chihuahua2.tecnm.mx>
2024-11-07 13:52:58 -0700

    Add files via upload

---
+++ EVA2_7_WHILE/src/main/java/com/mycompany/eva2_7_while/EVA2_7_WHILE.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_7_while;
+
+/**
+ *
+ * @author obeds
+ */
+public class EVA2_7_WHILE {
+
+    public static void main(String[] args) {
+        int n=1;
+        while(n<=1){
+            System.out.println(n +" ");
+            //n++;
+        }
+    }
+}
