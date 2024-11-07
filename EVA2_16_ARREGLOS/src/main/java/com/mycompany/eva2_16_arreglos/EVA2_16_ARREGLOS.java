11bed1b79eeda2dd46d211ddbca4041d011207ae
obed-27 <l24550299@chihuahua2.tecnm.mx>
2024-11-07 13:57:40 -0700

    Add files via upload

---
+++ EVA2_16_ARREGLOS/src/main/java/com/mycompany/eva2_16_arreglos/EVA2_16_ARREGLOS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_16_arreglos;
+
+/**
+ *
+ * @author obeds
+ */
+public class EVA2_16_ARREGLOS {
+
+    public static void main(String[] args) {
+        String[] diasSemana = {"domingo","lunes","martes","miercoes","jueves","viernes","sabado"};
+    }
+}
