64944d72a94527e2ee03cc872735c9ab10b41483
obed-27 <l24550299@chihuahua2.tecnm.mx>
2024-11-07 13:55:32 -0700

    Add files via upload

---
+++ EVA2_10_VALIDAR_2/src/main/java/com/mycompany/eva2_10_validar_2/EVA2_10_VALIDAR_2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_10_validar_2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author obeds
+ */
+public class EVA2_10_VALIDAR_2 {
+
+    public static void main(String[] args) {
+        int di;
+        Scanner mtr = new Scanner(System.in);
+        while(true){
+            System.out.println("numero correspondiente del dia");
+            di=mtr.nextInt();
+        }
+    }
+}
